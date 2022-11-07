272d9e30bc01b606ac59659a8481a484c463d3ee
kaiquecamposdesouza <kaique.souza@aluno.faculdadeimpacta.com.br>
2022-11-06 23:18:37 -0300

    deploy de templates-> primeiros templates

--- venv/controller.py
+++ venv/controller.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/inicio')
 def inicio():
-    return '<h1>Hello World!</h1>'
+    return render_template('lista.html', titulo='leandro')
 
 app.run()
