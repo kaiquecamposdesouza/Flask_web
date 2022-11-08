80125b8f90c2f1771f3db1d8c53af4095bc82a2e
kaiquecamposdesouza <kaique.souza@aluno.faculdadeimpacta.com.br>
2022-11-08 08:21:59 -0300

    deploy da classe jogos e inserção de informações dinamicas do backend para o frontend

--- venv/controller.py
+++ venv/controller.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template
 
+
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
 app = Flask(__name__)
 
 @app.route('/inicio')
 def inicio():
-    return render_template('lista.html', titulo='leandro')
+    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+    jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+    lista_jogos = [jogo1, jogo2]
 
-app.run()
+    return render_template('lista.html', titulo='leandro', jogos=lista_jogos)
+
+app.run(debug=True)
