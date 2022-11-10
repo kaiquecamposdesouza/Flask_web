e339687b293ad7550d569d50f1e022650adbd90e
kaiquecamposdesouza <kaique.souza@aluno.faculdadeimpacta.com.br>
2022-11-10 08:08:38 -0300

    criação de formulario para cadastro de novos jogos, ainda incompleto e com erro

--- venv/controller.py
+++ venv/controller.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 class Jogo:
@@ -7,19 +7,30 @@ class Jogo:
         self.categoria = categoria
         self.console = console
 
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+jogo3 = Jogo('Mortal Combate', 'Luta', 'PS2')
+lista_jogos = [jogo1, jogo2, jogo3]
+
 app = Flask(__name__)
 
 @app.route('/inicio')
 def inicio():
-    jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
-    jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
-    jogo3 = Jogo('Mortal Combate', 'Luta', 'PS2')
-    lista_jogos = [jogo1, jogo2, jogo3]
-
     return render_template('lista.html', titulo='leandro', jogos=lista_jogos)
 
 @app.route('/novo')
 def novo():
     return render_template('novo.html', titulo='novo jogo')
+
+@app.route('/criar',  methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista_jogos.append(jogo)
+
+    return render_template('lista.html', titulo='Jogos', jogos=lista_jogos)
+
 
 app.run(debug=True)
